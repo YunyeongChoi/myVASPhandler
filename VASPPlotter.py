61b9d997d1be799e4466dc9a113756e9ef36520a
yychoi <flyingyunyeong@gmail.com>
2020-03-21 18:20:48 -0700

    Slight polishing

--- VASPPlotter.py
+++ VASPPlotter.py
@@ -22,7 +22,7 @@ class Plotter(object):
         params = {'axes.linewidth' : 1.5,
                   'axes.unicode_minus' : False,
                   'figure.dpi' : 300,
-                  'font.size' : 35,
+                  'font.size' : 40,
                   'legend.frameon' : False,
                   'legend.handletextpad' : 0.4,
                   'legend.handlelength' : 1,
@@ -100,23 +100,33 @@ class Plotter(object):
                     if iteration == 0:
                         previous_key = '0'
                         previous_value = totaldictionary[previous_key]
-                        voltage = -((totaldictionary[i][ionnum] - previous_value) / (float(ionnum) - float(previous_key)) - ionvalues['ionenergy'][i])/ionvalues['ionoxidation'][i]
-                        plt.plot([0, ionnum/max(totaldictionary[i].keys())], [voltage, voltage], c = ionvalues['ioncolor'][i], label = i)
+                        voltage = -((totaldictionary[i][ionnum] - previous_value)
+                                    / (float(ionnum) - float(previous_key)) - ionvalues['ionenergy'][i])/ionvalues['ionoxidation'][i]
+                        plt.plot([0, ionnum/max(totaldictionary[i].keys())], 
+                                 [voltage, voltage], 
+                                 c = ionvalues['ioncolor'][i], label = i)
                         voltagelist.append(voltage)
                         previous_voltage = voltage
                         previous_key = ionnum
                         previous_value = totaldictionary[i][ionnum]       
                     else:
-                        voltage = -((totaldictionary[i][ionnum] - previous_value) / (float(ionnum) - float(previous_key)) - ionvalues['ionenergy'][i])/ionvalues['ionoxidation'][i]
-                        plt.plot([previous_key/max(totaldictionary[i].keys()), ionnum/max(totaldictionary[i].keys())], [voltage, voltage], c = ionvalues['ioncolor'][i])
-                        plt.plot([previous_key/max(totaldictionary[i].keys()), previous_key/max(totaldictionary[i].keys())], [previous_voltage, voltage], c = ionvalues['ioncolor'][i])
+                        voltage = -((totaldictionary[i][ionnum] - previous_value)
+                                    / (float(ionnum) - float(previous_key)) - ionvalues['ionenergy'][i])/ionvalues['ionoxidation'][i]
+                        plt.plot([previous_key/max(totaldictionary[i].keys()), 
+                                  ionnum/max(totaldictionary[i].keys())], 
+                                 [voltage, voltage], c = ionvalues['ioncolor'][i])
+                        plt.plot([previous_key/max(totaldictionary[i].keys()), 
+                                  previous_key/max(totaldictionary[i].keys())], 
+                                 [previous_voltage, voltage], c = ionvalues['ioncolor'][i])
                         voltagelist.append(voltage)
                         previous_voltage = voltage
                         previous_key = ionnum
                         previous_value = totaldictionary[i][ionnum]
                         
                     
-            plt.plot([0, 1], [sum(voltagelist)/len(voltagelist), sum(voltagelist)/len(voltagelist)], c = ionvalues['ioncolor'][i], ls='--')
+            plt.plot([0, 1], 
+                     [sum(voltagelist)/len(voltagelist), sum(voltagelist)/len(voltagelist)], 
+                     c = ionvalues['ioncolor'][i], ls='--')
 
         plt.legend()
         plt.xlim(xlim)
@@ -124,7 +134,7 @@ class Plotter(object):
         plt.xlabel(xlabel)
         plt.ylabel(ylabel)
         plt.title(self.calc_dir.split('/')[-1] + ' ' + spgp, y=1.01)
-        # plt.savefig('/global/cscratch1/sd/yychoi/JCESR/MgPostSpinels/Voltage/' + self.calc_dir.split('/')[-1] + "_" + spgp + '.png')
+        plt.savefig('/global/cscratch1/sd/yychoi/JCESR/MgPostSpinels/Voltage/' + self.calc_dir.split('/')[-1] + "_" + spgp + '.png')
         plt.show()
 
         return
@@ -190,11 +200,15 @@ class Plotter(object):
     def avplotter(self, spgp, workingion1, workingion2,
                  ionenergy = {'Na' : -1.3384, 'Mg' : -1.6043, 'Li' : -1.9080},
                  ionoxidation = {'Na' : 1, 'Mg' : 2, 'Li' : 1},
-                 ioncolor = {'Na' : 'darkgreen', 'Mg' : 'navy', 'Li' : 'firebrick'}):
+                 ioncolor = {'Na' : 'darkgreen', 'Mg' : 'navy', 'Li' : 'firebrick'},
+                 calstructure = {'Co0.5Sn1.5O4': {'color': 'royalblue'}, 'CrSnO4': {'color': 'darkorange'}, 'CrTiO4': {'color': 'forestgreen'}, 
+                                 'Fe0.5Ti1.5O4': {'color': 'dimgray'}, 'MnSnO4': {'color': 'firebrick'}, 'Ni0.5Sn1.5O4': {'color': 'darkorchid'},
+                                 'MnTiO4': {'color': 'darkkhaki'}}):
         
         self.set_rc_params()
-        calstructure = {'Co0.5Sn1.5O4': {'color': 'royalblue'}, 'CrSnO4': {'color': 'darkorange'}, 'CrTiO4': {'color': 'forestgreen'}, 
-                        'Fe0.5Ti1.5O4': {'color': 'dimgray'}, 'MnSnO4': {'color': 'firebrick'}, 'Ni0.5Sn1.5O4': {'color': 'darkorchid'}}
+        mpl.rcParams['figure.figsize'] = [18,18]
+        mpl.rcParams['font.size'] = 60
+        mpl.rcParams['legend.fontsize'] = 50
         caldir = []
         ion1volt = []
         ion2volt = []
@@ -222,29 +236,27 @@ class Plotter(object):
                         ion2dict[0] = a.Etot()
                     else:
                         print('Check the ions properly')
-                        
-            if label == 'Co0.5Sn1.5O4':
-                voltage1 = 0
-                voltage2 = 0
-            else:       
-                ion1dict = dict(sorted(ion1dict.items()))
-                ion2dict = dict(sorted(ion2dict.items()))
-                voltage1 = -(ion1dict[max(ion1dict, key=int)] - ion1dict[min(ion1dict, key=int)] - max(ion1dict, key=int) * ionenergy[workingion1]) / max(ion1dict, key=int) / ionoxidation[workingion1]
-                voltage2 = -(ion2dict[max(ion2dict, key=int)] - ion2dict[min(ion2dict, key=int)] - max(ion2dict, key=int) * ionenergy[workingion2]) / max(ion2dict, key=int) / ionoxidation[workingion2]
-                if i == 0:
-                    plt.bar(label, voltage1, width=0.5, color=ioncolor[workingion1], label=workingion1)
-                    plt.bar(label, voltage2-voltage1, width=0.5, bottom=voltage1, color=ioncolor[workingion2], label=workingion2)
-                else:
-                    plt.bar(label, voltage1, width=0.5, color=ioncolor[workingion1])
-                    plt.bar(label, voltage2-voltage1, width=0.5, bottom=voltage1, color=ioncolor[workingion2])
-                print(voltage1)
-                print(voltage2)
-
+                             
+            ion1dict = dict(sorted(ion1dict.items()))
+            ion2dict = dict(sorted(ion2dict.items()))
+            voltage1 = -(ion1dict[max(ion1dict, key=int)] - ion1dict[min(ion1dict, key=int)] 
+                         - max(ion1dict, key=int) * ionenergy[workingion1]) / max(ion1dict, key=int) / ionoxidation[workingion1]
+            voltage2 = -(ion2dict[max(ion2dict, key=int)] - ion2dict[min(ion2dict, key=int)] 
+                         - max(ion2dict, key=int) * ionenergy[workingion2]) / max(ion2dict, key=int) / ionoxidation[workingion2]
+            if i == 0:
+                plt.bar(label, voltage1, width=0.5, color=ioncolor[workingion1], label=workingion1)
+                plt.bar(label, voltage2-voltage1, width=0.5, bottom=voltage1, color=ioncolor[workingion2], label=workingion2)
+            else:
+                plt.bar(label, voltage1, width=0.5, color=ioncolor[workingion1])
+                plt.bar(label, voltage2-voltage1, width=0.5, bottom=voltage1, color=ioncolor[workingion2])
+            print(voltage1)
+            print(voltage2)
+        
         plt.legend()
-        plt.tick_params(axis='x', which='major', labelsize=18) # Move to method input
-        plt.title('Average Voltage' + "_" + spgp, y=1.01)
+        plt.tick_params(axis='x', which='major', labelsize=24) # Move to method input
+        plt.title('Average Voltage' + "_" + spgp, y=1.02)
         plt.ylabel('Voltage(V)')
-        # plt.savefig('/global/cscratch1/sd/yychoi/JCESR/MgPostSpinels/Voltage/' + self.calc_dir.split('/')[-1] + "Voltages_" + spgp + '.png')
+        plt.savefig('/global/cscratch1/sd/yychoi/JCESR/MgPostSpinels/Voltage/' + self.calc_dir.split('/')[-1] + "Voltages_" + spgp + '.png')
         
         return
 
@@ -291,76 +303,72 @@ class Plotter(object):
         
         if show == True:
             mpl.rcParams['font.size'] = 20
-            mpl.rcParams['xtick.labelsize'] = 15
-            mpl.rcParams['ytick.labelsize'] = 15
+            mpl.rcParams['xtick.labelsize'] = 30
+            mpl.rcParams['ytick.labelsize'] = 30
             mpl.rcParams['legend.fontsize'] = 15
-            plt.subplots_adjust(hspace=0.000, wspace=-1.000)
-            fig = plt.figure(figsize=(20,12))
+            mpl.rcParams['figure.figsize'] = [20,12]
             
-            for i, j in enumerate(caldir):
-                ax = plt.subplot(int('1' + str(len(caldir)) + str(i+1)))
-                
-                if 'lobster' in doscar:
-                    Efermi = 0.
-                else:
-                    Efermi = VASPBasicAnalysis(j).Efermi(alphabeta=False)
-                if shift == 'Fermi':                
-                    shift_value = -VASPBasicAnalysis(j).Efermi(alphabeta=False)
-                if normalization == 'electron':
-                    normalization = VASPBasicAnalysis(j).params_from_outcar(num_params=['NELECT'], str_params=[])['NELECT']
-                elif normalization == 'atom':
-                    normalization = VASPBasicAnalysis(j).nsites()
-                occupied_up_to = Efermi + shift_value
-                print(occupied_up_to)
-                print(Efermi)
-                print(shift_value)
-                dos_lw = 1    
-                for element in what_to_plot:
-                    for spin in what_to_plot[element]['spins']:
-                        for orbital in what_to_plot[element]['orbitals']:
-                            tag = '-'.join([element, spin, orbital])
-                            color = colors_and_labels[tag]['color']
-                            label = colors_and_labels[tag]['label']
-                            d = VASPDOSAnalysis(j, doscar=doscar).energies_to_populations(element=element, orbital=orbital, spin=spin)
-                            if spin == 'down':
-                                flip_sign = True
-                            else:
-                                flip_sign = False
-                            d = ProcessDOS(d, shift=shift_value, 
-                                           flip_sign=flip_sign,
-                                           normalization=normalization,
-                                           cb_shift=cb_shift,
-                                           vb_shift=vb_shift).energies_to_populations
+        for i, j in enumerate(caldir):
+            ax = plt.subplot(int('1' + str(len(caldir)) + str(i+1)))
+            if 'lobster' in doscar:
+                Efermi = 0.
+            else:
+                Efermi = VASPBasicAnalysis(j).Efermi(alphabeta=False)
+            if shift == 'Fermi':                
+                shift_value = -VASPBasicAnalysis(j).Efermi(alphabeta=False)
+            if normalization == 'electron':
+                normalization = VASPBasicAnalysis(j).params_from_outcar(num_params=['NELECT'], str_params=[])['NELECT']
+            elif normalization == 'atom':
+                normalization = VASPBasicAnalysis(j).nsites()
+            occupied_up_to = Efermi + shift_value
+            print(occupied_up_to, Efermi, shift_value)
+            dos_lw = 1    
+            for element in what_to_plot:
+                for spin in what_to_plot[element]['spins']:
+                    for orbital in what_to_plot[element]['orbitals']:
+                        tag = '-'.join([element, spin, orbital])
+                        color = colors_and_labels[tag]['color']
+                        label = colors_and_labels[tag]['label']
+                        d = VASPDOSAnalysis(j, doscar=doscar).energies_to_populations(element=element, orbital=orbital, spin=spin)
+                        if spin == 'down':
+                            flip_sign = True
+                        else:
+                            flip_sign = False
+                        d = ProcessDOS(d, shift=shift_value, 
+                                        flip_sign=flip_sign,
+                                        normalization=normalization,
+                                        cb_shift=cb_shift,
+                                        vb_shift=vb_shift).energies_to_populations
                                
-                            energies = sorted(list(d.keys()))
-                            populations = [d[E] for E in energies]
-                            occ_energies = [E for E in energies if E <= occupied_up_to]
-                            occ_populations = [d[E] for E in occ_energies]
-                            unocc_energies = [E for E in energies if E > occupied_up_to]
-                            unocc_populations = [d[E] for E in unocc_energies]    
-                            if smearing:
-                                occ_populations = gaussian_filter1d(occ_populations, smearing)
-                                unocc_populations = gaussian_filter1d(unocc_populations, smearing)
-                            ax = plt.plot(occ_populations, occ_energies, color=color, label=label, alpha=0.9, lw=dos_lw)
-                            ax = plt.plot(unocc_populations, unocc_energies, color=color, label='__nolegend__', alpha=0.9, lw=dos_lw)                
-                            ax = plt.fill_betweenx(occ_energies, occ_populations, color=color, alpha=0.2, lw=0)                               
-                
-                ax = plt.xticks(xticks[1])
-                ax = plt.yticks(yticks[1])
-                if xticks[0] == False:
-                    ax = plt.gca().xaxis.set_ticklabels([])      
-                if yticks[0] == False or i!=0:
-                    ax = plt.gca().yaxis.set_ticklabels([])
-                if i == 2:
-                    ax = plt.xlabel(xlabel)
-                if i == 0:
-                    ax = plt.ylabel(ylabel)
-                ax = plt.xlim(xlim)
-                ax = plt.ylim(ylim)    
-                if legend:
-                    ax = plt.legend(loc='upper right')
-                    ax = plt.title(j.split('/')[-1].split('_')[0], y=1.01)
-            if show:
-                    # plt.show()
-                plt.savefig('/global/cscratch1/sd/yychoi/JCESR/MgPostSpinels/Voltage/' + 'DOStest' + '.png')
-            return ax
+                        energies = sorted(list(d.keys()))
+                        populations = [d[E] for E in energies]
+                        occ_energies = [E for E in energies if E <= occupied_up_to]
+                        occ_populations = [d[E] for E in occ_energies]
+                        unocc_energies = [E for E in energies if E > occupied_up_to]
+                        unocc_populations = [d[E] for E in unocc_energies]    
+                        if smearing:
+                            occ_populations = gaussian_filter1d(occ_populations, smearing)
+                            unocc_populations = gaussian_filter1d(unocc_populations, smearing)
+                        ax = plt.plot(occ_populations, occ_energies, color=color, label=label, alpha=0.9, lw=dos_lw)
+                        ax = plt.plot(unocc_populations, unocc_energies, color=color, label='__nolegend__', alpha=0.9, lw=dos_lw)                
+                        ax = plt.fill_betweenx(occ_energies, occ_populations, color=color, alpha=0.2, lw=0)                               
+            ax = plt.xticks(xticks[1])
+            ax = plt.yticks(yticks[1])
+            ax = plt.subplots_adjust(hspace=0.000, wspace=0.000)
+            if xticks[0] == False:
+                ax = plt.gca().xaxis.set_ticklabels([])      
+            if yticks[0] == False or i!=0:
+                ax = plt.gca().yaxis.set_ticklabels([])
+            if i == 2:
+                ax = plt.xlabel(xlabel)
+            if i == 0:
+                ax = plt.ylabel(ylabel)
+            ax = plt.xlim(xlim)
+            ax = plt.ylim(ylim)    
+            if legend:
+                ax = plt.legend(loc='upper right')
+                ax = plt.title(j.split('/')[-1].split('_')[0], y=1.01)
+        if show:
+            plt.savefig('/global/cscratch1/sd/yychoi/JCESR/MgPostSpinels/Voltage/' + 'DOStest' + '.png')
+            plt.show()
+        return ax
